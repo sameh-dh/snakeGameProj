b2fde7ac6a82f859537caf9ea4f382e6cdd15ec1
sameh-dh <samehdh44@gmail.com>
2023-07-31 17:19:16 +0100

    image added to the grid

--- Images.cs
+++ Images.cs
@@ -7,12 +7,17 @@ namespace SnakeGameProj
 {
     public static class Images
     {
-        public readonly static ImageSource Empty = LoadImage("Empty");
+        public readonly static ImageSource Empty = LoadImage("Empty.png");
+        public readonly static ImageSource Body = LoadImage("Body.png");
+        public readonly static ImageSource Head = LoadImage("Head.png");
+        public readonly static ImageSource Food = LoadImage("Food.png");
+        public readonly static ImageSource DeadBody = LoadImage("DeadBody");
+        public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
    
     //Method LoadIMage
     private static ImageSource LoadImage(string fileName)
     {
-        return new BitmapImage(new Uri($"Assets/{fileName}",UriKind.Relative));
+        return new BitmapImage(new Uri($"Asset/{fileName}",UriKind.Relative));
     }
 
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,43 +22,56 @@ namespace SnakeGameProj
     {
 
         private readonly int rows = 15, cols = 15;
-       
+        private readonly Image[,] gridImages;
      
         public MainWindow()
         {
             InitializeComponent();
-            SetupGrid();
+            gridImages= SetupGrid();
+            Console.WriteLine("Hollllllaaaa");
+           
         }
 
         //Event 
         private  void Window_Loaded(object sender , RoutedEventArgs e)
         {
-          
-         
+            Draw();
+
         }
 
         //Draw mthod
         private void Draw()
         {
-           
+            DrawGrid();
         }
         //DrawGrid Method
-        private void SetupGrid()
+        private void DrawGrid()
         {
+            
+        }
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
             GameGrid.Rows = rows;
             GameGrid.Columns = cols;
             for (int r=0; r<rows; r++)
             {
                 for (int c=0; c<cols; c++)
                 {
-
-               
-                Border GridContent = new Border();
-                GridContent.Background = Brushes.White;
-                GameGrid.Children.Add(GridContent);
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+                    images[r, c] = image;
+                GameGrid.Children.Add(image);
 
                 }
             }
+            Console.WriteLine(images[0, 1]) ;
+            return images;
         }
+
+      
     }
+ 
 }
